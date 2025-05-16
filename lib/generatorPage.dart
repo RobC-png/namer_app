c9a463709677f664b2925711c67c5a241f23d20f
RobinPC <robin.walz.wien@gmail.com>
2025-05-16 22:21:23 +0200

    Remove unused import from generatorPage.dart

--- lib/generatorPage.dart
+++ lib/generatorPage.dart
@@ -2,7 +2,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/services.dart';
 import 'dart:io';
 
 //files
